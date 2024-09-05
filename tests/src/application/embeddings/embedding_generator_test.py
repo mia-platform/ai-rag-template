6d546cf5902957d4b37053eef4567cf1e9ec4045
ThisIsDemetrio <Demetrio Marino>
2024-09-05 11:36:06 +0200

    another approach to mock

--- tests/src/application/embeddings/embedding_generator_test.py
+++ tests/src/application/embeddings/embedding_generator_test.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 from unittest.mock import patch
+
+import requests
 import requests_mock
 
 from src.application.embeddings.embedding_generator import EmbeddingGenerator
+
 
 def test_generate(app_context):
     current_dir = Path(__file__).parent
@@ -10,15 +13,18 @@ def test_generate(app_context):
     with open(file_path, 'r', encoding='utf-8') as f:
         html_content = f.read()
 
-    with requests_mock.Mocker() as mock_requests, \
-        patch("langchain_experimental.text_splitter.SemanticChunker.split_text") as mock_split_text, \
+    # Initializing mock
+    session = requests.Session()
+    adapter = requests_mock.Adapter()
+
+    session.mount('http://', adapter)
+    adapter.register_uri('GET', 'http://example.com', text=html_content)
+
+    with patch("langchain_experimental.text_splitter.SemanticChunker.split_text") as mock_split_text, \
         patch('langchain_community.vectorstores.mongodb_atlas.MongoDBAtlasVectorSearch.add_documents') as mock_add_documents:
-
-        embedding_generator = EmbeddingGenerator(app_context)
-
-        mock_requests.get("http://example.com", text=html_content)
         mock_split_text.return_value = ["chunk1", "chunk2"]
 
+        embedding_generator = EmbeddingGenerator(app_context)
         embedding_generator.generate("http://example.com")
 
         mock_split_text.assert_called_once()
