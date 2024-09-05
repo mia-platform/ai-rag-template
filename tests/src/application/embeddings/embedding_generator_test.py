f751f6ff1132164f63c99c5d6c4140a074293bf9
ThisIsDemetrio <Demetrio Marino>
2024-09-05 11:21:32 +0200

    tentative

--- tests/src/application/embeddings/embedding_generator_test.py
+++ tests/src/application/embeddings/embedding_generator_test.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 from unittest.mock import patch
+import requests_mock
 
 from src.application.embeddings.embedding_generator import EmbeddingGenerator
-
 
 def test_generate(app_context):
     current_dir = Path(__file__).parent
@@ -10,13 +10,13 @@ def test_generate(app_context):
     with open(file_path, 'r', encoding='utf-8') as f:
         html_content = f.read()
 
-    with patch('requests.get') as mock_requests_get, \
+    with requests_mock.Mocker() as mock_requests, \
         patch("langchain_experimental.text_splitter.SemanticChunker.split_text") as mock_split_text, \
         patch('langchain_community.vectorstores.mongodb_atlas.MongoDBAtlasVectorSearch.add_documents') as mock_add_documents:
 
         embedding_generator = EmbeddingGenerator(app_context)
 
-        mock_requests_get.return_value.text = html_content
+        mock_requests.get("http://example.com", text=html_content)
         mock_split_text.return_value = ["chunk1", "chunk2"]
 
         embedding_generator.generate("http://example.com")
