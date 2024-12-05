6ff279a53dc679d28cbb292e513f1771789a275e
ThisIsDemetrio <Demetrio Marino>
2024-12-05 17:31:02 +0100

    feat: created manager to wrap llm and embedding classes

---
+++ src/infrastracture/llm_manager/llm_manager.py
@@ -0,0 +1,30 @@
+from langchain_openai import AzureChatOpenAI, ChatOpenAI
+from langchain_openai.chat_models.base import BaseChatOpenAI
+from src.infrastracture.llm_manager.errors import UnsupportedLlmProviderError
+from src.context import AppContext
+
+
+class LlmManager:
+    def __init__(self, app_context: AppContext):
+        self.app_context = app_context
+
+    def get_llm_instance(self) -> BaseChatOpenAI:
+        llm_api_key = self.app_context.env_vars.LLM_API_KEY
+        llm_configuration = self.app_context.configurations.llm
+
+        match llm_configuration.type:
+            case "openai":
+                return ChatOpenAI(
+                    openai_api_key=llm_api_key, 
+                    model=llm_configuration.name
+                )
+            case "azure":
+                return AzureChatOpenAI(
+                    api_key=llm_api_key,
+                    api_version=llm_configuration.apiVersion,
+                    azure_deployment=llm_configuration.deploymentName,
+                    azure_endpoint=llm_configuration.url,
+                    model=llm_configuration.name
+                )
+            case _:
+                raise UnsupportedLlmProviderError(llm_configuration.type)
