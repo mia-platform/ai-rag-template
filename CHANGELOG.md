5fc2e73fc3fab075275033cb2972afa03b4de0b3
ThisIsDemetrio <Demetrio Marino>
2024-12-19 16:21:20 +0100

    upgrade: service v0.5.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## 0.5.0 - 2024-12-19
+
 ### Added
 
 - Created new pipeline flow for testing, linting, security (with `bandit` and `pip-audit`) and docker image publishing on tags.
--- src/app.py
+++ src/app.py
@@ -22,7 +22,7 @@ def create_app(context: AppContext) -> FastAPI:
         openapi_url="/documentation/json",
         redoc_url=None,
         title="ai-rag-template",
-        version="0.4.0"
+        version="0.5.0"
     )
 
     app.add_middleware(AppContextMiddleware, app_context=context)
