0b767e42d3613350be24ad53e8d1b4b42f14904f
demetrio.marino <demetrio.marino@mia-platform.eu>
2026-01-01 18:40:43 +0100

    fix: conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,6 @@
-# pylint: disable=W0611
 # NOTE: clear_prometheus is needed to clear the prometheus registry before each test run in order to avoid conflicts between tests.
+from tests.fixtures.clear_prometheus import clear_prometheus_registry
+from tests.fixtures.test_client import test_client
+from tests.fixtures.mock_server import mock_server
+from tests.fixtures.logger import fixture_logger
+from tests.fixtures.app_context import app_context
